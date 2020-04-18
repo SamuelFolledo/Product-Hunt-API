0d50af22df0010ec9b191eb26c430b5161657219
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-18 16:55:41 -0400

    [add] feedVC

--- ProductHunt/FeedVC.swift
+++ ProductHunt/FeedVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FeedVC.swift
 //  ProductHunt
 //
 //  Created by Macbook Pro 15 on 4/15/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
